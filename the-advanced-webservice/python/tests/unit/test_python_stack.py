af8af4d44be2b808dc253cf487934933c7fd941d
Greg Farr <cybercaptain@duck.com>
2024-06-03 15:48:37 +0200

    added some unit tests

--- the-advanced-webservice/python/tests/unit/test_python_stack.py
+++ the-advanced-webservice/python/tests/unit/test_python_stack.py
@@ -1,15 +1,118 @@
-import aws_cdk as core
 import aws_cdk.assertions as assertions
+import pytest
 
-from python.python_stack import PythonStack
+from aws_cdk.assertions import Match
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in python/python_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = PythonStack(app, "python")
+from aws_cdk import (
+    Duration,
+    App,
+    Stack,
+    Tags,
+    aws_lambda as lambda_,
+    aws_rds as rds,
+)
+
+
+from the_advanced_webservice.the_advanced_webservice_stack import (
+    TheAdvancedWebserviceStack,
+)
+
+test_props = {
+    "createvpc": True,
+    "vpcAz": 2,
+    "createlambda": True,
+    "createhostedzone": False,
+    "createrdscluster": True,
+    "lambdaCodeFilePath": "./function/app.py.zip",
+    "runTime": lambda_.Runtime.PYTHON_3_12,
+    "databaseEngine": rds.DatabaseClusterEngine.aurora_mysql(
+        version=rds.AuroraMysqlEngineVersion.VER_3_06_0
+    ),
+    "existingDomainName": "xxxxxxx.awsps.myinstance.com",
+    # existingHostedZoneId: "XXXXXXXXXXXXXXXX",
+    "apiName": "myapi",
+    "stageName": "dev",
+    "originPath": "/dev",
+}
+
+
+def test_synthesizes_properly():
+    app = App()
+    stack = TheAdvancedWebserviceStack(app, "TheAdvancedWebserviceStack")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    #### creates API Gateway resources with...
+
+    # the expected number of API Gateway APIs
+    template.resource_count_is("AWS::ApiGateway::RestApi", 1)
+
+    # the expected number of API Gateway Stages
+    template.resource_count_is("AWS::ApiGateway::Stage", 2)
+
+    # the expected number of API Gateway deployments
+    template.resource_count_is("AWS::ApiGateway::Deployment", 2)
+
+    #### creates Lambda resources with...
+
+    # the expected number of Lambda functions
+    template.resource_count_is("AWS::Lambda::Function", 1)
+
+    #### creates RDS resources with...
+
+    # the expected number of RDS Clusters
+    template.resource_count_is("AWS::RDS::DBCluster", 1)
+
+    # the correct properties
+    template.has_resource_properties(
+        "AWS::RDS::DBCluster",
+        {
+            "CopyTagsToSnapshot": True,
+            "DatabaseName": "demos",
+            "Engine": "aurora-mysql",
+            "EngineVersion": "8.0.mysql_aurora.3.06.0",
+            "Port": 3306,
+            "ServerlessV2ScalingConfiguration": {"MaxCapacity": 64, "MinCapacity": 2},
+            "StorageEncrypted": True,
+            "StorageType": "aurora",
+            "VpcSecurityGroupIds": [{"Fn::GetAtt": [Match.anyValue(), "GroupId"]}],
+            "MasterUserPassword": Match.anyValue(),
+            "MasterUsername": Match.anyValue(),
+        },
+    )
+
+    # the expected number of RDS instances
+    template.resource_count_is("AWS::RDS::DBInstance", 2)
+
+    # the expected number of RDS proxies
+    template.resource_count_is("AWS::RDS::DBProxy", 1)
+
+    #### creates VPC resources with
+    # the expected number of VPCs
+    template.resource_count_is("AWS::EC2::VPC", 1)
+
+    # the correct properties
+    template.has_resource_properties(
+        "AWS::EC2::VPC",
+        {
+            "CidrBlock": "10.0.0.0/16",
+            "EnableDnsHostnames": True,
+            "EnableDnsSupport": True,
+            "InstanceTenancy": "default",
+            Tags: Match.arrayWith(
+                [
+                    {
+                        "Key": "Name",
+                        "Value": "TheAdvancedWebserviceStack/ServerlessApp/testvpc",
+                    }
+                ]
+            ),
+        },
+    )
+
+    # the expected number of VPC subnets
+    template.resource_count_is("AWS::EC2::Subnet", 4)
+
+    #### creates CloudFront resources with
+
+    # the expected number of Distriutions
+    template.resource_count_is("AWS::CloudFront::Distribution", 1)
